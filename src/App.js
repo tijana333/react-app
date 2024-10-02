fa981223ec63c58a7863d2c64e25a78ba452d8ee
tijana333 <150601653+tijana333@users.noreply.github.com>
2024-10-02 12:32:14 +0200

    TLD-3: Creating To-Do and Display List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  //čuvanje to-do stavki
+  const [newTodo, setNewTodo] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewTodo((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    // azuriranje state-a sa unetim vrednostima
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //dodavanje novog todo-a i resetovanje unosa
+
+    setTodos([...todos, newTodo]);
+    setNewTodo({ title: "", description: "" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          value={newTodo.title}
+          onChange={handleChange}
+          placeholder="Title"
+          required
+        />
+
+        <input
+          type="text"
+          name="description"
+          value={newTodo.description}
+          onChange={handleChange}
+          placeholder="Description"
+          required
+        />
+
+        <button type="submit">Add To-Do</button>
+      </form>
+      <h2>Your To-Do List:</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <h3>{todo.title} </h3>
+            <p>{todo.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
